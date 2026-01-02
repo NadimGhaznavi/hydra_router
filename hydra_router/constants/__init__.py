f1a76e9da61ae828697cb6e354f699ae72840779
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-01-02 16:24:00 -0500

    feat(constants): add constants package __init__.py with proper exports
    - Created hydra_router/constants/__init__.py to properly export constant classes
    - Exports DHydraLog, DMsgType, and DRouter for clean package imports
    - Enables 'from hydra_router.constants import DMsgType' style imports
    - Improves package structure and import organization

---
+++ hydra_router/constants/__init__.py
@@ -0,0 +1,15 @@
+"""
+Constants package for Hydra Router.
+
+This package contains all constant definitions used throughout the system.
+"""
+
+from .DHydraLog import DHydraLog
+from .DMsgType import DMsgType
+from .DRouter import DRouter
+
+__all__ = [
+    "DHydraLog",
+    "DMsgType",
+    "DRouter",
+]
