64b3573c648dffb608109b908b00d6b781a29d36
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-01-07 14:57:06 -0500

    Pre Release v0.12.23 - Accelerate

--- hydra_router/__init__.py
+++ hydra_router/__init__.py
@@ -14,7 +14,7 @@ This package provides HydraServer and HydraClient classes for simple
 REQ/REP messaging patterns using ZeroMQ.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.12.23"
 
 # Classes will be available after implementation
 __all__ = ["HydraServer", "HydraClient"]
--- hydra_router/constants/DHydra.py
+++ hydra_router/constants/DHydra.py
@@ -20,7 +20,7 @@ class DHydra:
     used throughout the HydraRouter package.
     """
 
-    VERSION: str = "0.12.22"
+    VERSION: str = "0.12.23"
 
 
 # HydraServer defaults
